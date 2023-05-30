826776aa18ca1f6fba5c8a78000f9d2902770088
OrejiNet <63667148+OrejiNet@users.noreply.github.com>
2023-05-29 23:14:09 -0400

    add user mapper

---
+++ src/main/java/com/usach/movie_backend/user/service/UserMapper.java
@@ -0,0 +1,29 @@
+package com.usach.movie_backend.user.service;
+
+import com.usach.movie_backend.user.domain.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper {
+
+    public User createUserMapping(UserCreate userCreate){
+        User user = new User();
+        user.setBirthday(userCreate.birthday());
+        user.setEmail(userCreate.email());
+        user.setFirstName(userCreate.firstName());
+        user.setLastName(userCreate.lastName());
+        user.setPassword(userCreate.password());
+        return user;
+    }
+
+    public User updateUserMapping(Integer idUser, UserUpdate userUpdate){
+        User user = new User();
+        user.setBirthday(userUpdate.birthday());
+        user.setEmail(userUpdate.email());
+        user.setFirstName(userUpdate.firstName());
+        user.setLastName(userUpdate.lastName());
+        user.setPassword(userUpdate.password());
+        user.setId(idUser);
+        return user;
+    }
+}
