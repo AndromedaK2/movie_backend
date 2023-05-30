212f901b93e90c00bad393ea07de740b9235e62a
CmoraGalaz <103866034+CmoraGalaz@users.noreply.github.com>
2023-05-30 15:02:00 -0400

    add Service Producer, Repository Producer, Controller Producer, Model Producer 30/05/2023

---
+++ src/main/java/com/usach/movie_backend/genders/service/IGenderService.java
@@ -0,0 +1,4 @@
+package com.usach.movie_backend.genders.service;
+
+public interface IGenderRepository {
+}
---
+++ src/main/java/com/usach/movie_backend/producers/controller/ProducerController.java
@@ -0,0 +1,2 @@
+package com.usach.movie_backend.producers.controller;public class ProducerController {
+}
