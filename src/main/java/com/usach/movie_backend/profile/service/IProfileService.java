791d51c9afd6f7ff601b6685b10ab38b109a9593
OrejiNet <63667148+OrejiNet@users.noreply.github.com>
2023-06-09 16:51:09 -0400

    add - create profile to a user

--- src/main/java/com/usach/movie_backend/profile/service/IProfileService.java
+++ src/main/java/com/usach/movie_backend/profile/service/IProfileService.java
@@ -1,17 +1,18 @@
 package com.usach.movie_backend.profile.service;
 
 import com.usach.movie_backend.profile.domain.Profile;
+import com.usach.movie_backend.profile.service.dtos.ProfileCreate;
 
 
 import java.util.List;
 import java.util.Optional;
 
-public interface IProfileService<T> {
+public interface IProfileService {
     List<Profile>findAll();
 
     Optional<Profile> findByProfile(Integer idProfile);
 
-    Profile create(Profile profile);
+    Profile create(ProfileCreate profileCreate, String userEmail );
 
     Profile update(Profile profile);
 
--- src/main/java/com/usach/movie_backend/profile/service/ProfileService.java
+++ src/main/java/com/usach/movie_backend/profile/service/ProfileService.java
@@ -3,37 +3,51 @@ package com.usach.movie_backend.profile.service;
 import com.usach.movie_backend.profile.domain.Profile;
 import com.usach.movie_backend.profile.repository.IProfileRepository;
 
+import com.usach.movie_backend.profile.service.dtos.ProfileCreate;
+import com.usach.movie_backend.user.domain.User;
+import com.usach.movie_backend.user.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 @Service
-public class ProfileService implements IProfileService<Profile> {
+public class ProfileService implements IProfileService{
     @Autowired
     private IProfileRepository profileRepository;
-    @Override
+    @Autowired
+    private IUserService userService;
+
+    @Transactional(readOnly = true)
     public List<Profile> findAll() {
         return profileRepository.findAll();
     }
 
-    @Override
+    @Transactional(readOnly = true)
     public Optional<Profile> findByProfile(Integer idProfile) {
         return profileRepository.findById(idProfile);
     }
 
-    @Override
-    public Profile create(Profile profile) {
+    @Transactional
+    public Profile create(ProfileCreate profileCreate, String userEmail) {
+        Optional<User> user = userService.findByEmail(userEmail);
+        Profile profile = new Profile();
+        profile.setUsername(profileCreate.username());
+        profile.setUrlPhoto(profileCreate.urlPhoto());
+        profile.setCreationDate(new Date());
+        profile.setUser(user.get());
         return profileRepository.save(profile);
     }
 
-    @Override
+    @Transactional
     public Profile update(Profile profile) {
         return profileRepository.save(profile);
     }
 
-    @Override
+    @Transactional
     public void delete(Integer id) {
         profileRepository.deleteById(id);
     }
