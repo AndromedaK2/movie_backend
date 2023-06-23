ef5addda63ed54c3a0a2b51417594ae83361d749
OrejiNet <63667148+OrejiNet@users.noreply.github.com>
2023-06-23 17:15:17 -0400

    fix - favorite repository

--- src/main/java/com/usach/movie_backend/favorites/repository/IFavoritesRepository.java
+++ src/main/java/com/usach/movie_backend/favorites/repository/IFavoritesRepository.java
@@ -2,11 +2,12 @@ package com.usach.movie_backend.favorites.repository;
 
 import com.usach.movie_backend.favorites.domain.Favorite;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import javax.swing.text.html.Option;
 import java.util.Optional;
 
 public interface IFavoritesRepository extends JpaRepository<Favorite, Integer> {
 
-    Optional<Favorite> findByNameAndIdProfile(String name, Integer idProfile);
+    Optional<Favorite> findByNameAndIdProfile(@Param("name") String name,@Param("idProfile") Integer idProfile);
 }
